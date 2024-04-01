0e61f32465722b7562bef7d718a201db248fa717
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-04-01 17:16:21 -0600

    Add dependencies

---
+++ src/Providers.tsx
@@ -0,0 +1,13 @@
+import { NextUIProvider } from '@nextui-org/react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+export default function Providers({ children }: Props) {
+    return (
+        <NextUIProvider>
+            {children}
+        </NextUIProvider>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Providers from './Providers.tsx'
 import App from './App.tsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>,
 )
