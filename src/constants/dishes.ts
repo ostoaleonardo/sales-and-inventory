49f3f61450fc838669fde711c9f06b796e8808aa
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-04-07 19:44:11 -0600

    Update dishes constants

--- src/constants/dishes.ts
+++ src/constants/dishes.ts
@@ -1,8 +1,7 @@
-export const INITIAL_VISIBLE_COLUMNS = [
-    'id',
+export const INITIAL_DISHES_COLUMNS = [
     'name',
     'price',
-    'category',
+    'categories',
     'actions'
 ]
 
@@ -10,14 +9,7 @@ export const DISHES_COLUMNS = [
     { name: 'ID', uid: 'id', sortable: true },
     { name: 'NOMBRE', uid: 'name', sortable: true },
     { name: 'PRECIO', uid: 'price', sortable: true },
-    { name: 'CATEGORIA', uid: 'category', sortable: true },
+    { name: 'CATEGORÍA', uid: 'categories', sortable: true },
     { name: 'CREADO', uid: 'created_at', sortable: true },
     { name: 'ACCIONES', uid: 'actions' },
-]
-
-export const DISHES_CATEGORIES = [
-    { name: 'Bebidas', uid: 'bebidas' },
-    { name: 'Platillos', uid: 'platillos' },
-    { name: 'Postres', uid: 'postres' },
-    { name: 'Extras', uid: 'extras' },
 ]
