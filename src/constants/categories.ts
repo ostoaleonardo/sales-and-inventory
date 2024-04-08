8e12ccd3f5045df587ad3f931b1cf65553688258
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-04-07 19:45:26 -0600

    Add constants for categories

---
+++ src/constants/categories.ts
@@ -0,0 +1,11 @@
+export const INITIAL_CATEGORIES_COLUMNS = [
+    'name',
+    'actions'
+]
+
+export const CATEGORIES_COLUMNS = [
+    { name: 'ID', uid: 'id', sortable: true },
+    { name: 'NOMBRE', uid: 'name', sortable: true },
+    { name: 'CREADO', uid: 'created_at', sortable: true },
+    { name: 'ACCIONES', uid: 'actions' },
+]
