3d3ebb2ec9bc48ab0401f1d18510ac22d6df0b69
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-04-07 19:46:30 -0600

    Add exports and constants for tables

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,21 @@
 export {
-    INITIAL_VISIBLE_COLUMNS,
-    DISHES_COLUMNS,
-    DISHES_CATEGORIES,
+    INITIAL_DISHES_COLUMNS,
+    DISHES_COLUMNS
 } from './dishes.ts'
+
+export {
+    INITIAL_CATEGORIES_COLUMNS,
+    CATEGORIES_COLUMNS,
+} from './categories.ts'
 
 export {
     NAVIGATION_ROUTES
 } from './routes.ts'
+
+export const TABLES = {
+    SALES: 'sales',
+    DISHES: 'dishes',
+    CATEGORIES: 'categories',
+}
 
 export const ROWS_PER_PAGE = ['5', '10', '15', '20', '30', '40', '50', 'Todas']
