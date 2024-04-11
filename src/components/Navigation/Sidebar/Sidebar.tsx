600bfe2ed164be0e64b834f40230dc8536684080
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-04-10 19:02:38 -0600

    Update sidebar to show user and role

--- src/components/Navigation/Sidebar/Sidebar.tsx
+++ src/components/Navigation/Sidebar/Sidebar.tsx
@@ -1,20 +1,46 @@
+import { Button } from '@nextui-org/react'
 import { SidebarHeader, SidebarLink } from '.'
+import { useAuth } from '@/hooks'
+import { LogOut } from '@/icons'
 import { NAVIGATION_ROUTES } from '@/constants'
 
 export function Sidebar() {
+    const { logOut } = useAuth()
+
     return (
         <aside className='w-20 xl:w-80 hidden sm:flex flex-col bg-zinc-950 gap-4 transition-width'>
             <SidebarHeader />
-            <div className='flex flex-col gap-2 px-2'>
-                {NAVIGATION_ROUTES.map((route, index) => (
-                    <SidebarLink
-                        key={index}
-                        href={route.path}
-                        icon={route.icon}
-                    >
-                        {route.name}
-                    </SidebarLink>
-                ))}
+            <div className='h-[85%] flex flex-col justify-between px-2'>
+                <div className='flex flex-col gap-2'>
+                    {NAVIGATION_ROUTES.map((route, index) => (
+                        <SidebarLink
+                            key={index}
+                            href={route.path}
+                            icon={route.icon}
+                        >
+                            {route.name}
+                        </SidebarLink>
+                    ))}
+                </div>
+
+                <Button
+                    size='lg'
+                    isIconOnly
+                    variant='light'
+                    onPress={logOut}
+                    className='w-full h-14 flex xl:hidden'
+                >
+                    <LogOut className='w-4 h-4' />
+                </Button>
+                <Button
+                    size='lg'
+                    variant='light'
+                    onPress={logOut}
+                    className='h-14 justify-start hidden xl:flex'
+                    startContent={<LogOut className='w-4 h-4' />}
+                >
+                    Cerrar sesión
+                </Button>
             </div>
         </aside>
     )
--- src/components/Navigation/Sidebar/SidebarHeader.tsx
+++ src/components/Navigation/Sidebar/SidebarHeader.tsx
@@ -1,13 +1,17 @@
+import { useAuth } from '@/hooks'
+
 export function SidebarHeader() {
+    const { user } = useAuth()
+
     return (
         <div className='w-full flex items-center max-xl:justify-center border-b-1 border-white/10 px-6 py-4'>
             <div className='w-10 bg-white/10 rounded-full aspect-square' />
             <div className='hidden xl:flex flex-col ml-4'>
                 <p className='font-semibold'>
-                    Hola, <span>Usuario</span>
+                    Hola, <span>{user?.email}</span>
                 </p>
-                <span className='text-xs opacity-75'>
-                    Empleado
+                <span className='text-xs opacity-75 capitalize'>
+                    {user?.role}
                 </span>
             </div>
         </div>
