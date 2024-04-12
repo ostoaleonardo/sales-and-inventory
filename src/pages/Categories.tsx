b11fe908461132a82d905d7bdbc712f79eced443
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-04-11 19:58:34 -0600

    Update categories page

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,6 +1,33 @@
+import { useState } from 'react'
+import { useDisclosure } from '@nextui-org/react'
+import { CategoriesModal, CategoriesTable } from '@/components'
+
 export function Categories() {
+    const [selectedCategory, setSelectedCategory] = useState<number | undefined>(undefined)
+    const { isOpen, onOpen, onOpenChange } = useDisclosure()
+
+    const onAdd = () => {
+        setSelectedCategory(undefined)
+        onOpen()
+    }
+
+    const onEdit = (id: number) => {
+        setSelectedCategory(id)
+        onOpen()
+    }
+
     return (
-        <main className='w-full h-screen'>
+        <main className='w-full h-full flex p-8'>
+            <CategoriesTable
+                onAdd={onAdd}
+                onEdit={onEdit}
+            />
+
+            <CategoriesModal
+                isOpen={isOpen}
+                onOpenChange={onOpenChange}
+                id={selectedCategory}
+            />
         </main>
     )
 }
