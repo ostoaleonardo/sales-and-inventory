41f908c6b6da1f8fdaa1a03a5e328ae1d48aa7e1
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-04-11 19:12:53 -0600

    Update constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -17,4 +17,13 @@ export {
     ROLES
 } from './database.ts'
 
-export const ROWS_PER_PAGE = ['5', '10', '15', '20', '30', '40', '50', 'Todas']
+export const ROWS_PER_PAGE = [
+    { value: '5', label: '5' },
+    { value: '10', label: '10' },
+    { value: '15', label: '15' },
+    { value: '20', label: '20' },
+    { value: '30', label: '30' },
+    { value: '40', label: '40' },
+    { value: '50', label: '50' },
+    { value: '-1', label: 'Todas' },
+]
