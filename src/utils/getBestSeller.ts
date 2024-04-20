8b52b1008ab78ee5befe4aa6da5709685f2e62d7
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-04-19 18:44:51 -0600

    Update utils functions

---
+++ src/utils/getBestSeller.ts
@@ -0,0 +1,18 @@
+export const getBestSeller = (sales: any) => {
+    return sales.reduce((acc: any, sale: any) => {
+        const { dishes, quantity } = sale
+        const { name, price } = dishes
+        const total = price * quantity
+
+        const existing = acc.find((item: any) => item.name === name)
+
+        if (existing) {
+            existing.quantity += quantity
+            existing.total += total
+        } else {
+            acc.push({ name, price, quantity, total })
+        }
+
+        return acc.sort((a: any, b: any) => b.quantity - a.quantity)
+    }, [])
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,12 @@
 export { getFormattedDate } from './getFormattedDate.ts'
+export { getDaysOfLastWeek } from './getDaysOfLastWeek.ts'
+export { getBestSeller } from './getBestSeller.ts'
+
+// Date ranges
+export {
+    now,
+    lastWeek,
+    currentWeek,
+    currentMonth,
+    emptyDateRange
+} from './getDateRange.ts'
